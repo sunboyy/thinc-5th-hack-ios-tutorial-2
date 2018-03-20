5818a165a0d367697388618a1b821fa342c2d42d
sunboyy <sura_sun@hotmail.com>
2018-03-20 21:21:59 +0700

    Initial Commit

---
+++ thinc-5th-hack-ios-tutorial-1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  thinc-5th-hack-ios-tutorial-1
+//
+//  Created by Smile on 3/20/2561 BE.
+//  Copyright © 2561 mrsunboy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
